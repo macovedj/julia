32482033800ba28b6edab3fb53e0f56ee064d6c1
Simeon Schaub <simeondavidschaub99@gmail.com>
2020-12-22 12:09:46 +0100

    use parent type in similar(::reshape/reinterpret)
    We already do this for `SubArray` and `PermutedDimsArray`, so I think this makes sense here.
    
    ref https://github.com/JuliaGPU/CUDA.jl/issues/602

--- base/reshapedarray.jl
+++ base/reshapedarray.jl
@@ -211,6 +211,8 @@ parentindices(A::ReshapedArray) = map(OneTo, size(parent(A)))
 reinterpret(::Type{T}, A::ReshapedArray, dims::Dims) where {T} = reinterpret(T, parent(A), dims)
 elsize(::Type{<:ReshapedArray{<:Any,<:Any,P}}) where {P} = elsize(P)
 
+similar(a::ReshapedArray, T::Type, d::Dims) = similar(a.parent, T, d)
+
 unaliascopy(A::ReshapedArray) = typeof(A)(unaliascopy(A.parent), A.dims, A.mi)
 dataids(A::ReshapedArray) = dataids(A.parent)
 
